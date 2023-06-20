e212bd4fcf0167bad267385c0b625f3e9135970b
Zarak <7254237+zarak@users.noreply.github.com>
2023-06-20 17:41:11 +0500

    Complete hittable

--- src/Hittable.hs
+++ src/Hittable.hs
@@ -1,9 +1,13 @@
 module Hittable where
 
+import Ray
 import Vec3
 
-data Hit = Hit
+data HitRecord = HitRecord
   { p :: Point,
     normal :: Vec3,
     t :: Double
   }
+
+class Hittable where
+  hit :: Ray -> Double -> Double -> HitRecord
