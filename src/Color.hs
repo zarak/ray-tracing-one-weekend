7b2600021f7f959706d23e96e6a265299612a8b7
Zarak <7254237+zarak@users.noreply.github.com>
2023-06-21 01:19:23 +0500

    Update writeColor

--- src/Color.hs
+++ src/Color.hs
@@ -1,5 +1,6 @@
 module Color where
 
+import RtWeekend
 import Text.Printf (printf)
 import Vec3
 
@@ -14,12 +15,26 @@ newtype Color = Color
   }
   deriving (Show, Num)
 
--- output RGB values scaled to [0,255]
-writeColor :: Color -> String
-writeColor (Color v) = printf "%d %d %d" (scaleColor v.x) (scaleColor v.y) (scaleColor v.z)
-  where
-    scaleColor :: Double -> Int
-    scaleColor color' = truncate (255.999 * color')
+white :: Color
+white = color 1 1 1
+
+black :: Color
+black = color 0 0 0
 
 color :: Red -> Green -> Blue -> Color
 color a b c = Color $ Vec3 a b c
+
+-- writeColor :: Color -> String
+-- writeColor (Color v) = printf "%d %d %d" (scaleColor v.x) (scaleColor v.y) (scaleColor v.z)
+--   where
+--     scaleColor :: Double -> Int
+--     scaleColor color' = truncate (255.999 * color')
+
+writeColor :: Vec3 -> Int -> String
+writeColor (Vec3 r g b) samplesPerPixel =
+  let scale = 1.0 / fromIntegral samplesPerPixel
+      r' = r * scale
+      g' = g * scale
+      b' = b * scale
+      toColor comp = floor (256 * clamp comp 0.0 0.999) :: Int
+   in printf "%d %d %d\n" (toColor r') (toColor g') (toColor b')
--- src/RtWeekend.hs
+++ src/RtWeekend.hs
@@ -1,7 +1,6 @@
 module RtWeekend where
 
 import Camera
-import Color
 
 ------------------------------------------------------------------------------
 -- Image
@@ -12,12 +11,6 @@ imageWidth = 400
 imageHeight :: Int
 imageHeight = truncate $ fromIntegral imageWidth / defaultCamera.aspectRatio
 
-white :: Color
-white = color 1 1 1
-
-black :: Color
-black = color 0 0 0
-
 ------------------------------------------------------------------------------
 -- Utility
 ------------------------------------------------------------------------------
@@ -25,7 +18,8 @@ degreesToRadians :: Double -> Double
 degreesToRadians deg = deg * pi / 180
 
 clamp :: Double -> Double -> Double -> Double
-clamp x min' max'
-  | x < min' = min'
-  | x > max' = max'
-  | otherwise = x
+clamp x low high = max low (min high x)
+
+------------------------------------------------------------------------------
+-- Random
+------------------------------------------------------------------------------
