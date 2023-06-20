3a78ffd1e6fe2b0a99ff51342701b1b444e4fcb2
Zarak <7254237+zarak@users.noreply.github.com>
2023-06-20 11:05:25 +0500

    Add progress indicator

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -1,6 +1,6 @@
 module MyLib (someFunc) where
 
-import Data.Foldable (traverse_)
+import System.IO (hFlush, hPutStr, stderr)
 import Text.Printf
 
 imageWidth :: Int
@@ -20,11 +20,28 @@ outputPixel (r, g, b) = printf "%d %d %d" (scaleColor r) (scaleColor g) (scaleCo
     scaleColor :: Double -> Int
     scaleColor color = truncate (255.999 * color)
 
+-- generate each line of the image
+generateLine :: Int -> IO ()
+generateLine j = do
+  let g = scale imageHeight j
+  putStrLn $
+    unlines
+      [ outputPixel (r, g, 0.25) | i <- [0 .. imageWidth - 1], let r = scale imageWidth i
+      ]
+
+-- recursive function to generate image and display progress
+generateImage :: Int -> IO ()
+generateImage 0 = do
+  hPutStr stderr "\rScanlines remaining: 0 \nDone.\n"
+  hFlush stderr
+generateImage j = do
+  hPutStr stderr ("\rScanlines remaining: " ++ show j ++ " ")
+  hFlush stderr
+  generateLine j
+  generateImage (j - 1)
+
 -- image generation
 someFunc :: IO ()
 someFunc = do
   putStrLn $ printf "P3\n%d %d\n255" imageWidth imageHeight
-  traverse_
-    putStrLn
-    [ outputPixel (r, g, 0.25) | j <- [imageHeight - 1, imageHeight - 2 .. 0], let g = scale imageHeight j, i <- [0 .. imageWidth - 1], let r = scale imageWidth i
-    ]
+  generateImage (imageHeight - 1)
