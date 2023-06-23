e6fb2553252e8dfbdef9e5e3d353a2ae06f45bb1
Zarak <7254237+zarak@users.noreply.github.com>
2023-06-23 13:54:04 +0500

    Position and orient camera

--- src/Camera.hs
+++ src/Camera.hs
@@ -6,10 +6,6 @@ import Ray
 import RtWeekend
 import Vec3
 
-------------------------------------------------------------------------------
--- Image
-------------------------------------------------------------------------------
-
 data Camera = Camera
   { origin :: Point,
     horizontal :: Vec3,
@@ -18,23 +14,24 @@ data Camera = Camera
   }
   deriving (Show)
 
-mkCamera :: Double -> Double -> Camera
-mkCamera vfov aspectRatio =
+mkCamera :: Point -> Point -> Vec3 -> Double -> Double -> Camera
+mkCamera lookfrom lookat vup vfov aspectRatio =
   let theta = degreesToRadians vfov
       h = tan $ theta / 2
       viewportHeight = 2.0 * h
       viewportWidth = aspectRatio * viewportHeight
 
-      focalLength = 1.0
+      w = unitVector $ lookat |-> lookfrom
+      u = unitVector $ cross vup w
+      v = cross w u
 
-      origin = Point zeros
-      horizontal = Vec3 viewportWidth 0 0
-      vertical = Vec3 0 viewportHeight 0
+      origin = lookfrom
+      horizontal = viewportWidth *^ u
+      vertical = viewportHeight *^ v
       lowerLeftCorner =
         origin.toVec3
           - horizontal ^/ 2
           - vertical ^/ 2
-          - Vec3 0 0 focalLength
    in Camera
         { origin = origin,
           horizontal = horizontal,
@@ -43,18 +40,15 @@ mkCamera vfov aspectRatio =
         }
 
 getRay :: Camera -> Double -> Double -> Ray
-getRay camera u v =
+getRay camera s t =
   Ray
     camera.origin
-    -- The direction vector of the ray goes from the origin of the camera to
-    -- a point on the screen with coordinates (u, v)
-    -- See https://raytracing.github.io/images/fig-1.03-cam-geom.jpg
     ( camera.origin
         |-> Point
           ( camera.lowerLeftCorner
-              + u
+              + s
               *^ camera.horizontal
-              + v
+              + t
               *^ camera.vertical
           )
     )
